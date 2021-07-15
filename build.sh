fecff909a9e91bab06adca5a756e00a984faa689
janson <janson@gmail.com>
2021-07-15 18:24:23 +0800

    change to 7.x

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@
 export X86_BIN=/projects/gogs-ks/ddnsto-c/build-x86/ddnsto
 export ARM_BIN=/projects/gogs-ks/ddnsto-c/build-arm/ddnsto
 
-./pkg.sh ddnsto_arm 6.0
-./pkg.sh ddnsto_arm 7.0
-./pkg.sh ddnsto_x86 6.0
-./pkg.sh ddnsto_x86 7.0
+./pkg.sh ddnsto_arm 6.x
+./pkg.sh ddnsto_arm 7.x
+./pkg.sh ddnsto_x86 6.x
+./pkg.sh ddnsto_x86 7.x
 
 cp ddnsto_x86.spk /home/janson/Downloads/honor/ddnsto/synology/
 cp ddnsto_arm.spk /home/janson/Downloads/honor/ddnsto/synology/
