635039ab59375f4a7534b826647ec7ef82e3ff9b
luyong <6luyong@gmail.com>
2021-07-15 15:20:48 +0800

    edit pkg.sh

--- pkg.sh
+++ pkg.sh
@@ -36,4 +36,4 @@ tar -czf $BUILDDIR/package.tgz *
 
 echo "3.生成$1.SPK"
 cd $BUILDDIR
-tar -cf $BUILDDIR/$1.spk package.tgz scripts WIZARD_UIFILES CHANGELOG INFO LICENSE PACKAGE_ICON.PNG
+tar -cf $BUILDDIR/$1.spk package.tgz scripts WIZARD_UIFILES CHANGELOG INFO LICENSE PACKAGE_ICON.PNG conf
