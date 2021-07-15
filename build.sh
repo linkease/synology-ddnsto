8c048ba547c78b4c3f7c8c6c1be884e793fe428f
janson <janson@gmail.com>
2021-07-15 18:25:43 +0800

    build ok for 7.x

--- build.sh
+++ build.sh
@@ -8,6 +8,8 @@ export ARM_BIN=/projects/gogs-ks/ddnsto-c/build-arm/ddnsto
 ./pkg.sh ddnsto_x86 6.x
 ./pkg.sh ddnsto_x86 7.x
 
-cp ddnsto_x86.spk /home/janson/Downloads/honor/ddnsto/synology/
-cp ddnsto_arm.spk /home/janson/Downloads/honor/ddnsto/synology/
+cp ddnsto_arm-6.x.spk /home/janson/Downloads/honor/ddnsto/synology/
+cp ddnsto_arm-7.x.spk /home/janson/Downloads/honor/ddnsto/synology/
+cp ddnsto_x86-6.x.spk /home/janson/Downloads/honor/ddnsto/synology/
+cp ddnsto_x86-7.x.spk /home/janson/Downloads/honor/ddnsto/synology/
 
