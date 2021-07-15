a493338136c0042d6b03e516ba937308203cc5a8
janson <janson@gmail.com>
2021-07-15 18:30:49 +0800

    dsm 7 and dsm 6 supported

--- build.sh
+++ build.sh
@@ -3,13 +3,13 @@
 export X86_BIN=/projects/gogs-ks/ddnsto-c/build-x86/ddnsto
 export ARM_BIN=/projects/gogs-ks/ddnsto-c/build-arm/ddnsto
 
-./pkg.sh ddnsto_arm 6.x
-./pkg.sh ddnsto_arm 7.x
-./pkg.sh ddnsto_x86 6.x
-./pkg.sh ddnsto_x86 7.x
+./pkg.sh ddnsto_arm dsm6
+./pkg.sh ddnsto_arm dsm7
+./pkg.sh ddnsto_x86 dsm6
+./pkg.sh ddnsto_x86 dsm7
 
-cp ddnsto_arm-6.x.spk /home/janson/Downloads/honor/ddnsto/synology/
-cp ddnsto_arm-7.x.spk /home/janson/Downloads/honor/ddnsto/synology/
-cp ddnsto_x86-6.x.spk /home/janson/Downloads/honor/ddnsto/synology/
-cp ddnsto_x86-7.x.spk /home/janson/Downloads/honor/ddnsto/synology/
+cp ddnsto_arm-dsm6.spk /home/janson/Downloads/honor/ddnsto/synology/
+cp ddnsto_arm-dsm7.spk /home/janson/Downloads/honor/ddnsto/synology/
+cp ddnsto_x86-dsm6.spk /home/janson/Downloads/honor/ddnsto/synology/
+cp ddnsto_x86-dsm7.spk /home/janson/Downloads/honor/ddnsto/synology/
 
